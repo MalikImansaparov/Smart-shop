6f13b86fb019aa0eb9e6677210eead3f8ab51350
imansaparov <malikimansaparov@gmail.com>
2021-07-29 15:12:58 +0600

    feat: add completed task

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import './main.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import {createBrowserHistory} from 'history'
+import thunk from 'redux-thunk'
+import {routerMiddleware, ConnectedRouter} from 'connected-react-router'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {Provider} from 'react-redux'
+
+import createRootReducer from 'reducers'
+import routes from 'routes'
+
+const history = createBrowserHistory()
+const middlewares = [thunk, routerMiddleware(history)]
+const store = createStore(
+  createRootReducer(history),
+  composeWithDevTools(applyMiddleware(...middlewares))
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      {routes}
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)
